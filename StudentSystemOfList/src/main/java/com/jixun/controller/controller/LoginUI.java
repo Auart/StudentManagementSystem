51e8ea81eba31590b39d779dbd8b8638651e7405
凹凸先森 <99536333+taibaibaby@users.noreply.github.com>
2022-02-20 07:45:43 +0800

    Add files via upload

---
+++ StudentSystemOfList/src/main/java/com/jixun/controller/controller/LoginUI.java
@@ -0,0 +1,19 @@
+package com.jixun.controller.controller;
+
+import com.jixun.controller.view.SystemUI;
+import com.jixun.service.Tool;
+
+import java.util.List;
+
+
+public class LoginUI implements SystemUI {
+
+    @Override
+    public List<String> printInfo() {
+        for (String s : LOGIN) {
+            System.out.print(s + "\n");
+            tempData.add(Tool.sc.nextLine());
+        }
+        return tempData;
+    }
+}
