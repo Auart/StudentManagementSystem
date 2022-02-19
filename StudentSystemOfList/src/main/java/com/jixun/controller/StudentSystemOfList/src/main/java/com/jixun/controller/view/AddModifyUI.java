1563a4635a0eb082dc9810a5be6a9f0997571ac6
凹凸先森 <99536333+taibaibaby@users.noreply.github.com>
2022-02-20 07:49:11 +0800

    Add files via upload

---
+++ StudentSystemOfList/src/main/java/com/jixun/controller/StudentSystemOfList/src/main/java/com/jixun/controller/view/AddModifyUI.java
@@ -0,0 +1,18 @@
+package com.jixun.controller.view;
+
+import com.jixun.controller.view.SystemUI;
+import com.jixun.service.Tool;
+
+import java.util.List;
+
+public class AddModifyUI implements SystemUI {
+
+    @Override
+    public List<String> printInfo() {
+        for (String s : FIELD) {
+            System.out.print("请输入学生" + s);
+            tempData.add(Tool.sc.nextLine());
+        }
+       return tempData;
+    }
+}
